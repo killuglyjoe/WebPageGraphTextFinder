afd54c9254e575260ee529d745bd609125190fd1
Unknown <killuglyjoe@gmail.com>
2017-09-05 10:18:40 +0300

    Update translations

--- wpgtf_en.ts
+++ wpgtf_en.ts
@@ -74,27 +74,28 @@
         <translation>&lt;span style=&quot; font-weight:600;&quot;&gt;Search begin for...&lt;/span&gt;</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="5"/>
+        <location filename="mainwindow.cpp" line="6"/>
         <source>&lt;span style=&quot; font-weight:600;&quot;&gt;Отмена&lt;/span&gt;</source>
         <translation>&lt;span style=&quot; font-weight:600;&quot;&gt;Cancel&lt;/span&gt;</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="6"/>
+        <location filename="mainwindow.cpp" line="8"/>
         <source>&lt;span style=&quot; font-weight:600;&quot;&gt;Конец&lt;/span&gt;</source>
         <translation>&lt;span style=&quot; font-weight:600;&quot;&gt;Finished&lt;/span&gt;</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="7"/>
+        <location filename="mainwindow.cpp" line="10"/>
         <source>&lt;span style=&quot; font-style:italic; color:#e83720;&quot;&gt;Сетевая ошибка адрес: %1 значение: %2 &lt;/span&gt;</source>
         <translation>&lt;span style=&quot; font-style:italic; color:#e83720;&quot;Network error for URL: %1 description: %2 &lt;/span&gt;</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="9"/>
+        <location filename="mainwindow.cpp" line="13"/>
         <source>&lt;span style=&quot; font-style:italic; text-decoration: underline; color:#0000ff;&quot;&gt;Найдено %1 совпадений по адресу %2 &lt;/span&gt;</source>
         <translation>&lt;span style=&quot; font-style:italic; text-decoration: underline; color:#0000ff;&quot;&gt;Found %1 matches in URL %2 &lt;/span&gt;</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="162"/>
+        <location filename="mainwindow.cpp" line="152"/>
+        <location filename="mainwindow.cpp" line="178"/>
         <source>Поиск текста &quot;%1&quot; для %2</source>
         <translation>Searching text &quot;%1&quot; for %2</translation>
     </message>
